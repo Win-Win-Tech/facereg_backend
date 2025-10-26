b6a981374d1320575d7ae8ab2b20a606eb22b37f
Five Y Technologies <142930108+Win-Win-Tech@users.noreply.github.com>
2025-10-27 01:41:10 +0530

    CORS_Domain_Allowed

--- facereg/facereg/settings.py
+++ facereg/facereg/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'facereg.wsgi.application'
 CORS_ALLOWED_ORIGINS = [
     "http://147.93.27.224",  # Nginx frontend
     "http://localhost:3000",
+    "https://gatekeeper.cloudgentechnologies.com",
 ]
 
 
